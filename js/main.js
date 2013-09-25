eda138845b150a3cab131d510a293162bfc6455d
Bernat Romagosa <bromagosa@dosta.net>
2013-09-25 13:36:31 +0200

    Styles and moving cat

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 function slideTo(cssClass) {
-	$('html, body').animate({
-    scrollTop: $(cssClass).offset().top
- }, 250);
-}
+	$('html, body').animate({ scrollTop: $(cssClass).offset().top - 46 }, 250);
+};
+
+function moveCat() {
+	var currentTop = parseInt($('.main-container').css("background-position").split(" ")[1]);
+
+	if (currentTop <= 0) {
+		$('.main-container').css({ "background-position": "100% " + $(window).height() + "px" })
+	} else {
+		$('.main-container').css({ "background-position": "100% " + (currentTop - 10) + "px" })
+	}
+};
+
+setInterval(moveCat,100);
